bcf0aeb55d0110e2adb521847768a0602b273c18
prasanthxpoffice <prasanthxpoffice@gmail.com>
2026-03-21 23:25:43 +0400

    feat: Introduce a suite of reusable UI components and implement the Genders master data page.

--- Data/UiGridColumn.cs
+++ Data/UiGridColumn.cs
@@ -2,9 +2,9 @@ using Microsoft.AspNetCore.Components;
 
 namespace HRMS.Data
 {
-    public class GridColumn<TItem>
+    public class UiGridColumn<TItem>
     {
-        public string Title { get; set; } = "";
+        public string Title { get; set; } = string.Empty;
         public string? FieldName { get; set; }
         public RenderFragment<TItem>? Template { get; set; }
     }
