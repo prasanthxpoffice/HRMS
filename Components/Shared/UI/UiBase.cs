f53b3ff671b3c21afe9b5d5cd3a3b0e2c5ddb261
prasanthxpoffice <prasanthxpoffice@gmail.com>
2026-03-27 03:01:55 +0400

    feat: Implement Genders master data management page and a shared `UiBase` component for common UI logic.

--- Components/Shared/UI/UiBase.cs
+++ Components/Shared/UI/UiBase.cs
@@ -8,6 +8,35 @@ public class UiBase : ComponentBase, IDisposable
     [Inject] protected LanguageService Lang { get; set; } = default!;
     [Inject] protected IResourceService Res { get; set; } = default!;
 
+    protected bool IsLoading { get; set; }
+    protected bool IsSaving { get; set; }
+
+    protected async Task LoadAsync(Func<Task> action)
+    {
+        try
+        {
+            IsLoading = true;
+            await action();
+        }
+        finally
+        {
+            IsLoading = false;
+        }
+    }
+
+    protected async Task SaveAsync(Func<Task> action)
+    {
+        try
+        {
+            IsSaving = true;
+            await action();
+        }
+        finally
+        {
+            IsSaving = false;
+        }
+    }
+
     protected override void OnInitialized()
     {
         Lang.OnLanguageChanged += HandleLanguageChange;
