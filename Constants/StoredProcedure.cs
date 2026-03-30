8e3ea5db0876358fb13b1a73a9fd86e4eb4978ea
prasanthxpoffice <prasanthxpoffice@gmail.com>
2026-03-31 02:07:37 +0400

    feat: add StoredProcedure constants for gender management operations

--- Constants/StoredProcedure.cs
+++ Constants/StoredProcedure.cs
@@ -6,7 +6,7 @@ namespace DB.Constants
         {
             public static class Master
             {
-                public const string spGenderGet = "master.spGenderGetb";
+                public const string spGenderGet = "master.spGenderGet";
                 public const string spGenderSave = "master.spGenderSave";
                 public const string spGenderDelete = "master.spGenderDelete";
             }
