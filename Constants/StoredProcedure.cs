e03ae7601236ee47e049dcb5a5cc0dc4ccb92ca9
prasanthxpoffice <prasanthxpoffice@gmail.com>
2026-04-05 22:08:08 +0400

    feat: add StoredProcedure constants class for HRMS master database operations

--- Constants/StoredProcedure.cs
+++ Constants/StoredProcedure.cs
@@ -7,8 +7,6 @@ namespace DB.Constants
             public static class Master
             {
                 public const string spGenderGet = "master.spGenderGet";
-                public const string spGenderSave = "master.spGenderSave";
-                public const string spGenderDelete = "master.spGenderDelete";
 
                 public const string spCompanyGet = "master.spCompanyGet";
                 public const string spCompanySave = "master.spCompanySave";
